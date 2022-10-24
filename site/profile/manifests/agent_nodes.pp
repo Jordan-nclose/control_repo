96bca96773a4229e1cf2683319b5869499164f74
Jordan-nclose <115461155+Jordan-nclose@users.noreply.github.com>
2022-10-24 12:13:13 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes{
   include dockeragent
-  doceragent::node{'web.puppet.vm':}
-  doceragent::node{'db.puppet.vm':}
+  dockeragent::node{'web.puppet.vm':}
+  dockeragent::node{'db.puppet.vm':}
 }
